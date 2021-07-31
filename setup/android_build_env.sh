f87e32c59871fcd5267b2a3d50b087809171f7a2
phoenix <62538135+phoenix-1708@users.noreply.github.com>
2021-07-31 07:25:27 +0530

    Update android_build_env.sh

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -13,13 +13,13 @@ DEBIAN_10_PACKAGES="libncurses5"
 PACKAGES=""
 
 echo "Adding GitHub apt key and repository!"
-apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
-apt-add-repository https://cli.github.com/packages
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
 
-apt-get update
+sudo apt update
 
 # Install lsb-core packages
-apt-get install lsb-core -y
+sudo apt install lsb-core -y
 
 LSB_RELEASE="$(lsb_release -d | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')"
 
@@ -31,8 +31,8 @@ elif [[ ${LSB_RELEASE} =~ "Debian GNU/Linux 10" ]]; then
     PACKAGES="${DEBIAN_10_PACKAGES}"
 fi
 
-DEBIAN_FRONTEND=noninteractive \
-    apt-get install \
+sudo DEBIAN_FRONTEND=noninteractive \
+    apt install \
     adb autoconf automake axel bc bison build-essential \
     ccache clang cmake expat fastboot flex g++ \
     g++-multilib gawk gcc gcc-multilib git gnupg gperf \
@@ -46,10 +46,10 @@ DEBIAN_FRONTEND=noninteractive \
     ${PACKAGES} -y
 
 echo -e "Setting up udev rules for adb!"
-curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules
-chmod 644 /etc/udev/rules.d/51-android.rules
-chown root /etc/udev/rules.d/51-android.rules
-systemctl restart udev
+sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules
+sudo chmod 644 /etc/udev/rules.d/51-android.rules
+sudo chown root /etc/udev/rules.d/51-android.rules
+sudo systemctl restart udev
 
 if [[ "$(command -v make)" ]]; then
     makeversion="$(make -v | head -1 | awk '{print $3}')"
@@ -60,5 +60,5 @@ if [[ "$(command -v make)" ]]; then
 fi
 
 echo "Installing repo"
-curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
-chmod a+rx /usr/local/bin/repo
+sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
+sudo chmod a+rx /usr/local/bin/repo
